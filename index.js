6e2a00e38be65abe62cc0b81731521e71a8f4341
purisquiri <mau.dorigo@outlook.es>
2020-11-16 15:52:39 -0500

    commit tree a680ad1ac70608a4e0263de33d82d1658113100b

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+ let names = ['Lisa', 'Kaitlin', 'Jan']
+ let holiday = "surprise"
+
+function writeCards(names, holiday ) {
+   
+    for (let i = 0; i < names.length; i++){
+        let array = []
+        return array.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`);
+        
+        
+        
+    }
+}
+
+function countDown(number) {
+    let countdown = number;
+    while (countdown >= 0) {
+        console.log(countdown--);
+    }
+}
